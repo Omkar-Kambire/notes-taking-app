16fb8770174f5067cd9ea603c65b68c1b701e4f9
Omkar Kambire <omkar.yupgp22@sinhgad.edu>
2025-10-03 19:40:20 +0530

    Creating tasks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const path =require("path")
+const path =require("path");
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -8,7 +9,15 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req,res) => {
-    res.render("index");
+    fs.readdir(`./files`, (err,files) => {
+        res.render("index",{files:files});
+    })
+})
+
+app.post('/create', (req,res) => {
+     fs.writeFile(`./files/${req.body.title.split(" ").join('')}.txt`,req.body.details, (err) => {
+        res.redirect('/');
+     })
 })
 
 app.listen(3000)
